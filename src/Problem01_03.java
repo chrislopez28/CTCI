08d9de4a01aca049441cdac39ec8fb0aa867c77b
Chris Lopez <chrislopez28@gmail.com>
2021-05-09 00:14:29 -0700

    Fixed Problem 1.3 to include a length parameter

--- src/Problem01_03.java
+++ src/Problem01_03.java
@@ -1,21 +1,24 @@
 public class Problem01_03 {
-    public static String urlify(String s) {
+    public static String urlify(String s, Integer length) {
         char[] charArr = s.toCharArray(); // Character Array so operation can be performed in-place
+        int count = 0;
         StringBuilder url = new StringBuilder(); // StringBuilder b/c String concatenation takes quadratic time O(n^2)
         for (char c : charArr) {
+            count++;
             if (Character.isWhitespace(c)) {
                 url.append("%20");
             } else {
                 url.append(c);
             }
+            if (count == length) break;
         }
         return url.toString();
     }
 
     public static void main(String[] args) {
-        String[] testCases = {"Boots and Cats", "Arrays and Strings", "nospace"};
-        for (String s : testCases) {
-            System.out.println("Test Case '" + s + "': " + urlify(s));
+        String[][] testCases = {{"Mr John Smith    ", "13"}, {"Boots and Cats", "14"}, {"Arrays and Strings", "18"}, {"nospace", "7"}};
+        for (String[] s : testCases) {
+            System.out.println("Test Case '" + s[0] + "': " + urlify(s[0], Integer.parseInt(s[1])));
         }
     }
 }
